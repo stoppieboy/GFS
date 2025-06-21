6cfeeef5aaafe98283e45ab5974106773e25ddf6
Shivam Verma <shivamv1332@gmail.com>
2025-06-21 17:05:55 +0530

    structure change

---
+++ internal/routers/WebServiceRouter.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WebServiceRoutes(r *mux.Router) {
+	r.HandleFunc("/nothing", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("nothing route"))
+	})
+}
